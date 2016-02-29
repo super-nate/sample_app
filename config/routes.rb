55f6db27f91d367ec30d3e8c5a302ff3ad955fbd
Zhiheng <zhiheng.xu@epassi.fi>
2016-02-29 08:32:25 +0200

    finished password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -16,7 +20,9 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  resources :account_activations, only: [:edit]
   resources :users
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
